bc279cfb313dc1bdf72216f7e81bc800ba44d519
TarasovaYuliya <tarasovayuliya1.19@gmail.com>
2020-04-21 10:49:44 +0300

    1. Создайте итерируемый объект, возвращающий генератор тридцати пяти чисел трибоначчи и выведите эти числа.

---
+++ labs/lab04/Threebonachi.py
@@ -0,0 +1,27 @@
+# 1. Создайте итерируемый объект, возвращающий генератор тридцати пяти чисел трибоначчи и выведите эти числа.
+
+size = input('Введите количество чисел трибоначчи для вывода: ')
+size = int(size)
+
+class GlobalValues:
+    L = [0, 0, 1]
+
+class Threebonachi:
+
+    def __init__(self):
+        i = 3
+
+        while i < size+1:
+            GlobalValues.L.append(GlobalValues.L[i - 1] + GlobalValues.L[i - 2] + GlobalValues.L[i - 3])
+            i += 1
+
+    def __iter__(self):
+        for i in range(size):
+            yield GlobalValues.L[i]
+
+
+List = Threebonachi()
+for number in List:
+    print(number, end=' ')
+
+
